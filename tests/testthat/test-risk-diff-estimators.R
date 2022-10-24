c1223b383ebcc8800f613a9ea1ab6c95d9880fe3
PhilBoileau <boileap2@gene.com>
2022-10-24 09:21:57 -0700

    tests for one-step TTE RD TEM VIP

--- tests/testthat/test-risk-diff-estimators.R
+++ tests/testthat/test-risk-diff-estimators.R
@@ -1,7 +1,8 @@
 source("testing-utils.R")
 
 test_that(
-  "one_step_estimator() produces accurate estimates without cross-fitting",
+  paste("one_step_estimator() produces accurate estimates without",
+        "cross-fitting (cont outcome)"),
   {
     library(sl3)
 
@@ -51,7 +52,7 @@ test_that(
 )
 
 test_that(
-  "one_step_estimator() solves the efficient influence function",
+  "one_step_estimator() solves the efficient influence function (cont outcome)",
   {
     library(sl3)
 
@@ -108,7 +109,8 @@ test_that(
 )
 
 test_that(
-  "tml_estimator() produces accurate estimates without cross-fitting",
+  paste("tml_estimator() produces accurate estimates without cross-fitting",
+        "(cont outcome)"),
   {
     library(sl3)
 
@@ -155,5 +157,207 @@ test_that(
 
     # note that the true parameter values for w_1, w_3 are 0, 1
     expect_equal(as.numeric(tmle_fit), c(0, 1), tolerance = 0.1)
+  }
+)
+
+test_that(
+  "one_step_estimator() solves the efficient influence function (cont outcome)",
+  {
+    library(sl3)
+
+    # generate data
+    set.seed(84891)
+    dt <- generate_test_data(n_obs = 00, outcome_type = "time-to-event")
+    long_dt <- tte_data_melt(
+      data = dt,
+      confounders = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      censoring = "censoring",
+      time_cutoff = 5,
+      prop_score_values = NULL
+    )
+
+    # fit the propensity score
+    prop_score_fit <- fit_prop_score(
+      train_data = dt,
+      valid_data = long_dt,
+      learners = sl3::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3")
+    )
+
+    # fit the expected failure hazard
+    fail_fit <- fit_failure_hazard(
+      train_data = long_dt,
+      valid_data = NULL,
+      learners = sl3:::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3")
+    )
+
+    # fit the expected failure hazard
+    cens_fit <- fit_censoring_hazard(
+      train_data = long_dt,
+      valid_data = NULL,
+      learners = sl3:::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3"),
+      censoring = "censoring"
+    )
+
+    # compute the uncentered eif
+    ueif_dt <- uncentered_eif(
+      data = long_dt,
+      type = "risk difference",
+      confounders = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      modifiers = c("w_1", "w_3"),
+      prop_score_fit = prop_score_fit,
+      cond_outcome_fit = NULL,
+      prop_score_values = NULL,
+      failure_hazard_fit = fail_fit,
+      censoring_hazard_fit = cens_fit
+    )
+
+
+    one_step_fit <- one_step_estimator(uncentered_eif_data = ueif_dt)
+
+    # note that the true parameter values for w_1, w_3 are 0, 1
+    expect_equal(c(
+      mean(ueif_dt$w_1 - one_step_fit$w_1),
+      mean(ueif_dt$w_3 - one_step_fit$w_3)
+    ),
+    c(0, 0),
+    tolerance = 1e-10
+    )
+  }
+)
+
+test_that(
+  paste("one_step_estimator() produces accurate estimates without",
+        "cross-fitting (time-to-event outcome, RD TEM VIP)"),
+  {
+    library(sl3)
+
+    # generate data
+    set.seed(84891)
+    dt <- generate_test_data(n_obs = 5000, outcome_type = "time-to-event")
+    long_dt <- tte_data_melt(
+      data = dt,
+      confounders = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      censoring = "censoring",
+      time_cutoff = 5,
+      prop_score_values = NULL
+    )
+
+    # fit the propensity score
+    prop_score_fit <- fit_prop_score(
+      train_data = dt,
+      valid_data = long_dt,
+      learners = sl3::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3")
+    )
+
+    # fit the expected failure hazard
+    fail_fit <- fit_failure_hazard(
+      train_data = long_dt,
+      valid_data = NULL,
+      learners = sl3:::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3")
+    )
+
+    # fit the expected failure hazard
+    cens_fit <- fit_censoring_hazard(
+      train_data = long_dt,
+      valid_data = NULL,
+      learners = sl3:::Lrnr_xgboost$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3"),
+      censoring = "censoring"
+    )
+
+    # compute the uncentered eif
+    ueif_dt <- uncentered_eif(
+      data = long_dt,
+      type = "risk difference",
+      confounders = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      modifiers = c("w_1", "w_3"),
+      prop_score_fit = prop_score_fit,
+      cond_outcome_fit = NULL,
+      prop_score_values = NULL,
+      failure_hazard_fit = fail_fit,
+      censoring_hazard_fit = cens_fit
+    )
+
+    # fit the propensity score
+    one_step_fit <- one_step_estimator(uncentered_eif_data = ueif_dt)
+
+    # note that the true parameter values for w_1, w_3 are approx 1.7, 0, 0
+    expect_equal(as.numeric(one_step_fit), c(1.7, 0, 0), tolerance = 0.1)
+  }
+)
+
+test_that(
+  "one_step_estimator() solves the efficient influence function (cont outcome)",
+  {
+    library(sl3)
+
+    # generate data
+    set.seed(84891)
+    dt <- generate_test_data(n_obs = 100)
+
+    # fit the propensity score
+    prop_score_fit <- fit_prop_score(
+      train_data = dt,
+      valid_data = NULL,
+      learners = sl3::Lrnr_glm_fast$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3")
+    )
+
+    # fit the expected cond outcome
+    cond_outcome_fit <- fit_cond_outcome(
+      train_data = dt,
+      valid_data = NULL,
+      learners = sl3::Lrnr_ranger$new(),
+      exposure = "a",
+      confounders = c("w_1", "w_2", "w_3"),
+      outcome = "y"
+    )
+
+    # compute the uncentered eif
+    ueif_dt <- uncentered_eif(
+      data = dt,
+      type = "risk difference",
+      confounders = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "y",
+      modifiers = c("w_1", "w_3"),
+      prop_score_fit = prop_score_fit,
+      prop_score_values = NULL,
+      cond_outcome_fit = cond_outcome_fit,
+      failure_hazard_fit = NULL,
+      censoring_hazard_fit = NULL
+
+    )
+
+    one_step_fit <- one_step_estimator(uncentered_eif_data = ueif_dt)
+
+    # note that the true parameter values for w_1, w_3 are 0, 1
+    expect_equal(c(
+      mean(ueif_dt$w_1 - one_step_fit$w_1),
+      mean(ueif_dt$w_3 - one_step_fit$w_3)
+    ),
+    c(0, 0),
+    tolerance = 1e-10
+    )
   }
 )
