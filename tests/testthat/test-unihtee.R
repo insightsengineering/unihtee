736a4e3e70b59b68cdc239dc34fe2ab351a74b42
PhilBoileau <boileap2@gene.com>
2022-10-26 11:00:25 -0700

    unihtee() supports TTE TEM VIP estimation

--- tests/testthat/test-unihtee.R
+++ tests/testthat/test-unihtee.R
@@ -111,7 +111,7 @@ test_that(
   {
 
     # generate data
-    set.seed(712535)
+    set.seed(823421)
     dt <- generate_test_data(n_obs = 5000, outcome_type = "binary")
 
     # apply unihtee
@@ -123,11 +123,13 @@ test_that(
       outcome = "y",
       outcome_type = "binary",
       risk_type = "risk difference",
-      estimator = "tmle"
+      estimator = "tmle",
+      cond_outcome_estimator = sl3::Lrnr_xgboost$new(),
+      prop_score_estimator = sl3::Lrnr_xgboost$new()
     )
 
-    # ensure that the adjusted p-value of w_3 is less than 0.05, and those of
-    # w_1 and w_2 are above 0.05
+    # ensure that the adjusted p-value of w_1 is less than 0.05, and those of
+    # w_2 and w_2 are above 0.05
     expect_equal(results$p_value_fdr < 0.05, c(TRUE, FALSE, FALSE))
 
     # check that the estimates are reasonably close to the ground truth
@@ -200,6 +202,86 @@ test_that(
 
     # check that the estimates are reasonably close to the ground truth
     expect_equal(results$estimate[1], 4.2, tolerance = 0.1)
+    expect_equal(results$estimate[2], 0, tolerance = 0.1)
+    expect_equal(results$estimate[3], 0, tolerance = 0.1)
+  }
+)
+
+test_that(
+  paste(
+    "unihtee() uncovers treatment effect modifiers on the risk diff scale",
+    "with time-to-event outcomes with the one-step estimator"
+  ),
+  {
+
+    # generate data
+    set.seed(72342)
+    dt <- generate_test_data(n_obs = 5000, outcome_type = "time-to-event")
+
+    # apply unihtee
+    results <- unihtee(
+      data = dt,
+      confounders = c("w_1", "w_2", "w_3"),
+      modifiers = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      censoring = "censoring",
+      time_cutoff = 5,
+      outcome_type = "time-to-event",
+      risk_type = "risk difference",
+      estimator = "onestep",
+      prop_score_estimator = sl3::Lrnr_xgboost$new(),
+      cond_outcome_estimator = NULL,
+      failure_hazard_estimator = sl3::Lrnr_xgboost$new(),
+      censoring_hazard_estimator = sl3::Lrnr_xgboost$new()
+    )
+
+    # ensure that the adjusted p-value of w_1 is less than 0.05, and those of
+    # w_2 and w_2 are above 0.05
+    expect_equal(results$p_value_fdr < 0.05, c(TRUE, FALSE, FALSE))
+
+    # check that the estimates are reasonably close to the ground truth
+    expect_equal(results$estimate[1], 1.72, tolerance = 0.1)
+    expect_equal(results$estimate[2], 0, tolerance = 0.1)
+    expect_equal(results$estimate[3], 0, tolerance = 0.1)
+  }
+)
+
+test_that(
+  paste(
+    "unihtee() uncovers treatment effect modifiers on the risk diff scale",
+    "with time-to-event outcomes with the TML estimator"
+  ),
+  {
+
+    # generate data
+    set.seed(72341)
+    dt <- generate_test_data(n_obs = 5000, outcome_type = "time-to-event")
+
+    # apply unihtee
+    results <- unihtee(
+      data = dt,
+      confounders = c("w_1", "w_2", "w_3"),
+      modifiers = c("w_1", "w_2", "w_3"),
+      exposure = "a",
+      outcome = "time",
+      censoring = "censoring",
+      time_cutoff = 5,
+      outcome_type = "time-to-event",
+      risk_type = "risk difference",
+      estimator = "tmle",
+      prop_score_estimator = sl3::Lrnr_xgboost$new(),
+      cond_outcome_estimator = NULL,
+      failure_hazard_estimator = sl3::Lrnr_xgboost$new(),
+      censoring_hazard_estimator = sl3::Lrnr_xgboost$new()
+    )
+
+    # ensure that the adjusted p-value of w_1 is less than 0.05, and those of
+    # w_2 and w_2 are above 0.05
+    expect_equal(results$p_value_fdr < 0.05, c(TRUE, FALSE, FALSE))
+
+    # check that the estimates are reasonably close to the ground truth
+    expect_equal(results$estimate[1], 1.72, tolerance = 0.1)
     expect_equal(results$estimate[2], 0, tolerance = 0.1)
     expect_equal(results$estimate[3], 0, tolerance = 0.1)
   }
